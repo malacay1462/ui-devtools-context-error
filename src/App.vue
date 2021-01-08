a51143b1d16c188e38b44d9d3786b51ffa4e1302
malacay1462 <malacay1462@googlemail.com>
2021-01-08 13:10:22 +0100

    New Demos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,75 @@
 <template>
   <v-app>
-    <v-main>
-      <div class="bg-gray-100 flex justify-center items-center w-full h-screen">
-        <div class="shadow w-64 rounded bg-white">
-          <div>Text</div>
+    <v-main class="bg-gray-100 w-full h-full dark">
+      <div class="flex justify-center items-center mt-5">
+        <div class="shadow rounded w-50% p-5 bg-white text-material-grey-base">
           <div>
-            <div class="text-material-red-lighten-5">Erin Lindford</div>
-            <div class="text-material-red-darken-2">Product Engineer</div>
+            <div class="text-material-brown-lighten-3">Erin Lindford</div>
+            <div class="text-black">Product Engineer</div>
             <div>erin.landford@acme.com</div>
             <div>(555) 765-4321</div>
           </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center">
+        <div class="shadow w-50% mt-4 rounded bg-white  p-5">
+          Demo with mixed classes:
+          <div class="bg-gray-400" :class="{ 'pl-2': usePadding }">
+            Demo Text (change this div)
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center">
+        <div class="shadow w-50% mt-4 rounded bg-white  p-5">
+          Demo with dark mode support:
+          <div class="bg-gray-400 dark:bg-gray-900 dark:text-white">
+            Demo Text (change this div)
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center">
+        <div class="shadow w-50% mt-4 rounded bg-white  p-5">
+          Demo ul with foreach & component - Style on li:
+          <ul class="list-disc text-sm mb-0" :class="{ 'pl-2': usePadding }">
+            <li
+              v-for="count in maxCount"
+              :key="count"
+              class="text-material-red-lighten-1"
+            >
+              <demo-text :count="count" />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center">
+        <div class="shadow w-50% mt-4 rounded bg-white p-5">
+          Demo ul with foreach & component - Style on component:
+          <ul class="list-disc text-sm mb-0" :class="{ 'pl-2': usePadding }">
+            <li v-for="count in maxCount" :key="count">
+              <demo-text
+                class="text-material-purple-lighten-1"
+                :count="count"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center">
+        <div class="shadow w-50% mt-4 rounded bg-white p-5 mb-5">
+          Demo ul with foreach & component - Style on component (condition):
+          <ul class="list-disc text-sm mb-0" :class="{ 'pl-2': usePadding }">
+            <li v-for="count in maxCount" :key="count">
+              <demo-text
+                :class="{ 'text-material-purple-lighten-1': otherColor }"
+                :count="count"
+              />
+            </li>
+          </ul>
         </div>
       </div>
     </v-main>
@@ -17,16 +77,19 @@
 </template>
 
 <script>
+import DemoText from "./components/DemoText.vue";
 
 export default {
   name: "App",
 
   components: {
-
+    DemoText
   },
 
   data: () => ({
-    //
+    maxCount: 7,
+    usePadding: true,
+    otherColor: true
   })
 };
 </script>
---
+++ src/components/DemoText.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Demo Text (component - style on parent): {{ count }}
+
+    <span class="text-material-deepPurple-base text-xs">Inline Style</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  props: {
+    count: {
+      type: Number
+    }
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
+
